b64fc6f76c5eb8ade5e1a103ee1ffeb390a091a7
Kirill-Bokov <kirill37reg2@gmail.com>
2026-02-01 16:33:19 +0300

    category fixes

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,31 +1,24 @@
-import { useParams, useNavigate, useLocation } from "react-router-dom"
+import { useParams, useNavigate, useSearchParams } from "react-router-dom"
+import { useSelector } from "react-redux"
 import {
   useGetCategoryAttributesQuery,
   useGetProductsByCategoryQuery,
 } from "../app/api/catalogApi"
-import { useSelector } from "react-redux"
+import { selectAppliedFiltersQuery } from "../app/selectors/filtersSelectors"
 import type { RootState } from "../app/store"
 import { DataLoader } from "../components/dataLoader/DataLoader"
 import { CategoryAttributes } from "../components/categoryAttributes/CategoryAttributes"
 import { ProductGrid } from "../components/productGrid/productGrid"
 import { skipToken } from "@reduxjs/toolkit/query/react"
-import { selectAppliedFiltersQuery } from "../app/selectors/filtersSelectors"
-import type { Product } from "../types/catalogTypes"
 
 export default function Category() {
-  const { categoryId, subcategoryId } = useParams<{
-    categoryId?: string
-    subcategoryId?: string
-  }>()
+  const { categoryId, subcategoryId } = useParams<{ categoryId?: string; subcategoryId?: string }>()
   const idToFetch = subcategoryId ?? categoryId
   const navigate = useNavigate()
-  const location = useLocation()
-  const searchResults = location.state?.searchResults as Product[] | undefined
-  const fromSearch = location.state?.fromSearch as boolean | undefined
+  const [searchParams] = useSearchParams()
+  const queryText = searchParams.get("q")
 
-  const appliedFiltersQuery = useSelector((s: RootState) =>
-    selectAppliedFiltersQuery(s)
-  )
+  const appliedFiltersQuery = useSelector((s: RootState) => selectAppliedFiltersQuery(s))
 
   const {
     data: attributes,
@@ -34,33 +27,28 @@ export default function Category() {
   } = useGetCategoryAttributesQuery(idToFetch ? Number(idToFetch) : skipToken)
 
   const queryArgs = idToFetch
-    ? {
-      categoryId: Number(idToFetch),
-      filters: appliedFiltersQuery,
-    }
+    ? { categoryId: Number(idToFetch), filters: appliedFiltersQuery }
     : skipToken
 
   const {
     data: products,
     isLoading: productsLoading,
     error: productsError,
-  } = useGetProductsByCategoryQuery(queryArgs, {
-    skip: fromSearch && !!searchResults,
-  })
-
-  const displayProducts = fromSearch && searchResults ? searchResults : products
+  } = useGetProductsByCategoryQuery(queryArgs)
 
   const handleVariantClick = (variantId: number) => {
     navigate(`/product/${variantId}`)
   }
 
-  console.log("Категория:", idToFetch)
-  console.log("Применённые фильтры:", appliedFiltersQuery)
-  console.log("Из поиска:", fromSearch)
-  console.log("Продукты:", displayProducts)
+  const showSearchQuery = queryText && queryText.length >= 2
 
   return (
-    <div className="container mx-auto px-4 py-6">
+    <div className="container mx-auto px-4 py-6 space-y-6">
+      {showSearchQuery && (
+        <p className="text-sm text-gray-500">
+          Результаты по запросу: <span className="font-medium">{queryText}</span>
+        </p>
+      )}
       <DataLoader
         isLoading={attributesLoading}
         error={attributesError}
@@ -69,15 +57,14 @@ export default function Category() {
       >
         {attributes && <CategoryAttributes attributes={attributes} />}
       </DataLoader>
-
       <DataLoader
-        isLoading={!fromSearch && productsLoading}
+        isLoading={productsLoading}
         error={productsError}
         loadingText="Загрузка товаров..."
         errorText="Ошибка при загрузке товаров"
       >
-        {displayProducts && (
-          <ProductGrid products={displayProducts} onVariantClick={handleVariantClick} />
+        {products && (
+          <ProductGrid products={products} onVariantClick={handleVariantClick} />
         )}
       </DataLoader>
     </div>
